b7bf0f9967cffd9001d372cfb8ea6d440c35b99b
Piyush Mehta <me@piyushmehta.com>
2020-09-25 20:11:05 +0530

    Jumbotron init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Jumbotron from './components/jumbotron';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <div className="App"><Jumbotron/></div>;
 }
 
 export default App;
---
+++ src/components/jumbotron.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Jumbotron = () => {
+    return (
+        <div>
+            I'm  a Jumbotron
+        </div>
+    )
+}
+
+export default Jumbotron
