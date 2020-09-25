6df66a7eb32c261775ee5f333b0dad99443fc723
Piyush Mehta <me@piyushmehta.com>
2020-09-25 23:13:29 +0530

    global style added

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./App";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import { GlobalStyles } from './global-styles';
 
-render(<App />, document.getElementById("root"));
+render(
+    <>
+        <GlobalStyles />
+        <App />
+    </>,
+    document.getElementById('root'),
+);
