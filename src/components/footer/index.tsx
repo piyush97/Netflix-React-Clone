3e4a6c353d3b3f253f037b6baee03619b1867f8e
Piyush Mehta <me@piyushmehta.com>
2020-09-26 22:57:23 +0530

    pre styles created and footer made happy

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Container } from './styles/footer';
 
-interface Props {}
-const Footer = ({}: Props) => {
-    return <></>;
+interface Props {
+    children?: object;
+    restProps?: object;
+}
+const Footer = ({ children, ...restProps }: Props) => {
+    return <Container {...restProps}>{children}</Container>;
 };
 export default Footer;
