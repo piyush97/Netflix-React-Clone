0f29588db622b108c5661a5363819c2d6fd074cf
Piyush Mehta <me@piyushmehta.com>
2020-09-27 12:53:21 +0530

    without type check, but with errors

---
+++ src/components/accordian/index.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState, useContext } from 'react';
+import { Container, Inner, Title, Frame, Item, Header, Body } from './styles/accordian';
+
+const ToggleContext = createContext();
+interface Props {
+    children?: any;
+    restProps?: any;
+}
+const Accordian = ({ children, ...restProps }: Props) => {
+    return (
+        <Container {...restProps}>
+            <Inner>{children}</Inner>
+        </Container>
+    );
+};
+Accordian.Title = ({ children, ...restProps }: Props) => {
+    return <Title {...restProps}>{children}</Title>;
+};
+Accordian.Frame = ({ children, ...restProps }: Props) => {
+    return <Frame {...restProps}>{children}</Frame>;
+};
+Accordian.Item = ({ children, ...restProps }: Props) => {
+    const [toggleShow, setToggleShow] = useState(false);
+    return (
+        <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+            <Item {...restProps}>{children}</Item>
+        </ToggleContext.Provider>
+    );
+};
+Accordian.Header = ({ children, ...restProps }: Props) => {
+    const { toggleShow, setToggleShow } = useContext(ToggleContext);
+    return (
+        <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+            {children}
+        </Header>
+    );
+};
+Accordian.Body = ({ children, ...restProps }: Props) => {
+    const { toggleShow } = useContext(ToggleContext);
+    return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+};
+export default Accordian;
