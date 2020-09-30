b8283cb47bddcfdda6cb40bdbfe81ee3ebd4c62c
Piyush Mehta <me@piyushmehta.com>
2020-10-01 00:17:21 +0530

    fix page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -20,32 +20,36 @@ const Signin: React.FC<Props> = () => {
     };
     return (
         <>
-            <HeaderContainer>Sign In</HeaderContainer>
-            <Form>
-                <Form.Title>Sign In</Form.Title>
-                {error && <Form.Error>{error}</Form.Error>}
-                <Form.Base onSubmit={handleSignIn} method="POST">
-                    <Form.Input
-                        placeholder="email"
-                        value={emailAddress}
-                        onChange={(target: any) => setEmailAddress(target.value)}
-                    />
-                    <Form.Input
-                        placeholder="Password"
-                        value={password}
-                        onChange={(target: any) => setPassword(target.value)}
-                    />
-                    <Form.Submit disabled={isInValid} type="submit">
-                        Submit
-                    </Form.Submit>
-                </Form.Base>
-                <Form.Link>
-                    <Form.Text>
-                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
-                    </Form.Text>
-                </Form.Link>
-            </Form>
-            <FooterContainer></FooterContainer>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+                    <Form.Base onSubmit={handleSignIn} method="POST">
+                        <Form.Input
+                            placeholder="email"
+                            value={emailAddress}
+                            onChange={(target: any) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input
+                            placeholder="Password"
+                            value={password}
+                            onChange={(target: any) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={isInValid} type="submit">
+                            Submit
+                        </Form.Submit>
+                    </Form.Base>
+                    <Form.Link>
+                        <Form.Text>
+                            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                        </Form.Text>
+                        <Form.TextSmall>
+                            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                        </Form.TextSmall>
+                    </Form.Link>
+                </Form>{' '}
+            </HeaderContainer>
+            <FooterContainer />
         </>
     );
 };
